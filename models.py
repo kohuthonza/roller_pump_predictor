Fix normalization, add export and new models.

models.py
@@ -1,5 +1,16 @@
+import torch
+import math
 from torch import nn
 from functools import partial
+
+
+class LinearProjection(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Linear(1000, 1000, bias=False)
+
+    def forward(self, x):
+        return self.net(x)
 
 
 class AffineProjection(nn.Module):
@@ -15,8 +26,9 @@ class SimpleNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = [nn.Linear(1000, 500),
+                    nn.BatchNorm1d(500),
                     nn.ReLU(),
-                    nn.Linear(500, 1000)]
+                    nn.Linear(500, 1000, bias=False)]
         self.net = nn.Sequential(*self.net)
 
     def forward(self, x):
@@ -27,12 +39,15 @@ class SimpleNet2(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = [nn.Linear(1000, 500),
+                    nn.BatchNorm1d(500),
                     nn.ReLU(),
                     nn.Linear(500, 500),
+                    nn.BatchNorm1d(500),
                     nn.ReLU(),
                     nn.Linear(500, 500),
+                    nn.BatchNorm1d(500),
                     nn.ReLU(),
-                    nn.Linear(500, 1000)]
+                    nn.Linear(500, 1000, bias=False)]
         self.net = nn.Sequential(*self.net)
 
     def forward(self, x):
@@ -43,12 +58,15 @@ class SimpleNet3(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = [nn.Linear(1000, 1000),
+                    nn.BatchNorm1d(1000),
                     nn.ReLU(),
                     nn.Linear(1000, 1000),
+                    nn.BatchNorm1d(1000),
                     nn.ReLU(),
                     nn.Linear(1000, 1000),
+                    nn.BatchNorm1d(1000),
                     nn.ReLU(),
-                    nn.Linear(1000, 1000)]
+                    nn.Linear(1000, 1000, bias=False)]
         self.net = nn.Sequential(*self.net)
 
     def forward(self, x):
@@ -70,11 +88,11 @@ class ConvNet(nn.Module):
                           nn.Conv1d(128, 256, 5, padding=2),
                           nn.BatchNorm1d(256),
                           nn.ReLU(),
-                          nn.Conv1d(256, 1, 1),
-                          nn.BatchNorm1d(1),
+                          nn.Conv1d(256, 5, 5, padding=2),
+                          nn.BatchNorm1d(5),
                           nn.ReLU()]
         self.conv_part = nn.Sequential(*self.conv_part)
-        self.fully_part = [nn.Linear(1000, 1000),
+        self.fully_part = [nn.Linear(5*1000, 1000),
                            nn.BatchNorm1d(1000),
                            nn.ReLU(),
                            nn.Linear(1000, 1000, bias=False)]
@@ -83,12 +101,115 @@ class ConvNet(nn.Module):
     def forward(self, x):
         x = x.unsqueeze(1)
         x = self.conv_part(x)
-        x = x[:, 0, :]
+        x = torch.flatten(x, start_dim=1)
         return self.fully_part(x)
 
 
-net_definitions = {'affine_projection': partial(AffineProjection),
+class ConvLSTMNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_part = [nn.Conv1d(1, 32, 5, padding=2),
+                          nn.BatchNorm1d(32),
+                          nn.ReLU(),
+                          nn.Conv1d(32, 64, 5, padding=2),
+                          nn.BatchNorm1d(64),
+                          nn.ReLU(),
+                          nn.Conv1d(64, 128, 5, padding=2),
+                          nn.BatchNorm1d(128),
+                          nn.ReLU(),
+                          nn.Conv1d(128, 256, 5, padding=2),
+                          nn.BatchNorm1d(256),
+                          nn.ReLU()]
+        self.conv_part = nn.Sequential(*self.conv_part)
+        self.recurrent_part = nn.LSTM(256, 128, 2, bidirectional=True)
+        self.aggregation_part = [nn.Conv1d(256, 5, 5, padding=2),
+                                 nn.BatchNorm1d(5),
+                                 nn.ReLU()]
+        self.aggregation_part = nn.Sequential(*self.aggregation_part)
+        self.fully_part = [nn.Linear(5*1000, 1000),
+                           nn.BatchNorm1d(1000),
+                           nn.ReLU(),
+                           nn.Linear(1000, 1000, bias=False)]
+        self.fully_part = nn.Sequential(*self.fully_part)
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x = self.conv_part(x)
+        x = x.permute(2, 0, 1)
+        x, _ = self.recurrent_part(x)
+        x = x.permute(1, 2, 0)
+        x = self.aggregation_part(x)
+        x = torch.flatten(x, start_dim=1)
+        return self.fully_part(x)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+
+class ConvAttentionNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_part = [nn.Conv1d(1, 32, 5, padding=2),
+                          nn.BatchNorm1d(32),
+                          nn.ReLU(),
+                          nn.Conv1d(32, 64, 5, padding=2),
+                          nn.BatchNorm1d(64),
+                          nn.ReLU(),
+                          nn.Conv1d(64, 128, 5, padding=2),
+                          nn.BatchNorm1d(128),
+                          nn.ReLU(),
+                          nn.Conv1d(128, 256, 5, padding=2),
+                          nn.BatchNorm1d(256),
+                          nn.ReLU()]
+        self.conv_part = nn.Sequential(*self.conv_part)
+        self.pos_encoder = PositionalEncoding(d_model=256, dropout=0, max_len=1000)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=256, nhead=4, dim_feedforward=1024, dropout=0)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=2)
+        self.aggregation_part = [nn.Conv1d(256, 5, 5, padding=2),
+                                 nn.BatchNorm1d(5),
+                                 nn.ReLU()]
+        self.aggregation_part = nn.Sequential(*self.aggregation_part)
+        self.fully_part = [nn.Linear(5*1000, 1000),
+                           nn.BatchNorm1d(1000),
+                           nn.ReLU(),
+                           nn.Linear(1000, 1000, bias=False)]
+        self.fully_part = nn.Sequential(*self.fully_part)
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x = self.conv_part(x)
+        x = x.permute(2, 0, 1)
+        x = self.pos_encoder(x)
+        x = self.transformer_encoder(x)
+        x = x.permute(1, 2, 0)
+        x = self.aggregation_part(x)
+        x = torch.flatten(x, start_dim=1)
+        return self.fully_part(x)
+
+
+net_definitions = {'linear_projection': partial(LinearProjection),
+                   'affine_projection': partial(AffineProjection),
                    'simple_net': partial(SimpleNet),
                    'simple_net_2': partial(SimpleNet2),
                    'simple_net_3': partial(SimpleNet3),
-                   'conv_net': partial(ConvNet)}
+                   'conv_net': partial(ConvNet),
+                   'conv_lstm_net': partial(ConvLSTMNet),
+                   'conv_attention_net': partial(ConvAttentionNet)}

train.py
@@ -25,6 +25,8 @@ def parseargs():
     parser.add_argument('--tst-num-workers', default=8, type=int)
     parser.add_argument('--trn-prefetch-factor', default=1, type=int)
     parser.add_argument('--tst-prefetch-factor', default=1, type=int)
+    parser.add_argument('--max-pressure', default=60000, type=float)
+    parser.add_argument('--max-speed', default=1.6, type=float)
 
     # Model definition
     #
@@ -47,7 +49,8 @@ def parseargs():
     parser.add_argument('-d', '--checkpoint-dir', default='.', type=str)
     parser.add_argument('--test-step', default=500, type=int)
     parser.add_argument('--save-step', default=500, type=int)
-    parser.add_argument('--show-dir', default='.', type=str)
+    parser.add_argument('--show-dir', type=str)
+    parser.add_argument('--export-dir', type=str)
 
     parser.add_argument('--view-step', type=int, help='Set test_step, save_step to view_step.')
 
@@ -60,14 +63,15 @@ def main():
 
     print("INIT DATASETS")
     print()
-    trn_dataset, tst_datasets = init_datasets(args.trn_wave_directory_path,
-                                              args.tst_wave_directory_path,
-                                              args.batch_size,
-                                              args.trn_num_workers,
-                                              args.tst_num_workers,
-                                              args.trn_prefetch_factor,
-                                              args.tst_prefetch_factor)
-    init_show_dirs(args.show_dir)
+    trn_dataset, tst_datasets = init_datasets(trn_wave_directory_path=args.trn_wave_directory_path,
+                                              tst_wave_directory_path=args.tst_wave_directory_path,
+                                              batch_size=args.batch_size,
+                                              max_pressure=args.max_pressure,
+                                              max_speed=args.max_speed,
+                                              trn_num_workers=args.trn_num_workers,
+                                              tst_num_workers=args.tst_num_workers,
+                                              trn_prefetch_factor=args.trn_prefetch_factor,
+                                              tst_prefetch_factor=args.tst_prefetch_factor)
 
     gpu_owner = GPUOwner()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -125,10 +129,12 @@ def main():
 
             if test_and_show:
                 model_wrapper.set_eval()
-                test(model_wrapper, iteration, trn_dataset, train=True, show_dir=args.show_dir)
+                test(model_wrapper, iteration, trn_dataset, train=True, show_dir=args.show_dir,
+                     export_dir=args.export_dir)
                 if tst_datasets:
                     for tst_dataset in tst_datasets:
-                        test(model_wrapper, iteration, tst_dataset, train=False, show_dir=args.show_dir)
+                        test(model_wrapper, iteration, tst_dataset, train=False, show_dir=args.show_dir,
+                             export_dir=args.export_dir)
                 model_wrapper.set_train()
                 print()
 
@@ -138,8 +144,8 @@ def main():
                 data = batch['input_pressure_wave']
                 net_speed = (args.test_step * data.shape[0]) / train_net_time
                 trn_loss /= args.test_step
-                print(f"TRAIN {iteration} ({total_nb_waves / 1000:.1f}k lines seen) loss:{trn_loss:.3f} \
-                           lr:{args.learning_rate} time:{train_time:.1f} net_speed:{net_speed}")
+                print(f"TRAIN {iteration} ({total_nb_waves / 1000:.1f}k waves seen) loss:{trn_loss:.3f}"
+                      f" lr:{args.learning_rate} time:{train_time:.1f} net_speed:{net_speed}")
                 print()
                 train_net_time = 0.0
                 trn_loss = 0.0
@@ -167,12 +173,12 @@ def main():
         if stop_training:
             break
 
-    training_time += time.time() - train_time
+    training_time += time.time() - train_timer
 
     print("AVERAGE TIME OF 100 ITERATIONS: {}".format((training_time / (args.max_iterations - args.start_iteration)) * 100))
 
 
-def test(model_wrapper, iteration, dataset, train, show_dir):
+def test(model_wrapper, iteration, dataset, train, show_dir, export_dir):
     total_loss = 0
     t1 = time.time()
     total_net_time = 0
@@ -189,18 +195,16 @@ def test(model_wrapper, iteration, dataset, train, show_dir):
 
             net_t1 = time.time()
             outputs, loss = model_wrapper.test_step(batch)
-
+            total_net_time += time.time() - net_t1
+            total_loss += loss.mean().item()
+            total_nb_waves += targets.shape[0]
             all_targets.append(targets)
             all_outputs.append(outputs)
             all_paths.append(paths)
 
-            total_loss += loss.mean().item()
-            total_net_time += time.time() - net_t1
-            total_nb_waves += targets.shape[0]
-
     t2 = time.time()
 
-    print('TEST {} {:d} loss:{:.5f} full_speed:{:.0f} net_speed:{:.0f} time:{:.1f}'.format(
+    print('TEST {} {:d} loss:{:.5f} full_speed:{:.0f} net_speed:{:.0f} time:{:.5f}'.format(
         dataset.dataset.wave_directory_path,
         iteration,
         total_loss / it_count,
@@ -209,7 +213,54 @@ def test(model_wrapper, iteration, dataset, train, show_dir):
         t2 - t1))
 
     if show_dir is not None:
-        show_images(all_targets, all_outputs, all_paths, iteration, dataset.dataset.wave_directory_path, train, show_dir)
+        show_waves(all_targets, all_outputs, all_paths, dataset.dataset.max_speed, iteration, dataset.dataset.wave_directory_path, train, show_dir)
+    if export_dir is not None:
+        export_waves(all_outputs, all_paths, dataset.dataset.max_speed, iteration, dataset.dataset.wave_directory_path, export_dir)
+
+
+def show_waves(all_targets, all_outputs, all_paths, max_speed, iteration, wave_directory_path, train, show_dir):
+    iteration_dir = os.path.join(show_dir, os.path.basename(wave_directory_path), str(iteration))
+    if not os.path.exists(iteration_dir):
+        os.makedirs(iteration_dir)
+
+    counter = 0
+    for targets, outputs, paths in zip(all_targets, all_outputs, all_paths):
+        for target, output, path in zip(targets, outputs, paths):
+            target = target.cpu().numpy()
+            output = output.cpu().numpy()
+            target += 1
+            target = target * (max_speed / 2.0)
+            output += 1
+            output = output * (max_speed / 2.0)
+            t = np.arange(0, 1, 0.001)
+
+            plt.plot(t, target, color='red', label='target')
+            plt.plot(t, output, color='blue', label='output')
+            plt.legend()
+            plt.savefig(os.path.join(iteration_dir, '{}.pdf'.format(path)))
+            plt.cla()
+            if train:
+                counter += 1
+            if counter == 40:
+                return
+
+
+def export_waves(all_outputs, all_paths, max_speed, iteration, wave_directory_path, export_dir):
+    iteration_dir = os.path.join(export_dir, os.path.basename(wave_directory_path), str(iteration))
+    if not os.path.exists(iteration_dir):
+        os.makedirs(iteration_dir)
+
+    for outputs, paths in zip(all_outputs, all_paths):
+        for output, path in zip(outputs, paths):
+            output = output.cpu().numpy()
+            output += 1
+            output = output * (max_speed / 2.0)
+            out_lines = []
+            with open(os.path.join(wave_directory_path, path)) as f:
+                for l, o in zip(f.readlines(), output):
+                    out_lines.append("{},{}\n".format(l.strip(), str(o)))
+            with open(os.path.join(iteration_dir, path), "w") as f:
+                f.writelines(out_lines)
 
 
 def init_datasets(trn_wave_directory_path, tst_wave_directory_path, batch_size, max_pressure=60000, max_speed=1.6,
@@ -234,63 +285,6 @@ def init_datasets(trn_wave_directory_path, tst_wave_directory_path, batch_size, 
 
     return trn_dataset, tst_datasets
 
-
-def show_images(all_targets, all_outputs, all_paths, iteration, wave_directory_path, train, show_dir):
-    """
-    images = []
-    data = data.detach().cpu().numpy()
-    data = np.transpose(data, (0, 2, 3, 1))
-    data *= 255
-    for i in range(data.shape[0]):
-        images.append(data[i])
-
-    image = np.concatenate(images, axis=0)
-
-    dataset_name = os.path.split(dataset_name)[-1]
-    train_test = "train"
-    if not train:
-        train_test = "test"
-
-    image_path = os.path.join(show_dir, "test", train_test, "TEST_BATCH_{}_{:06d}.jpg".format(dataset_name, iteration))
-
-    if not train:
-        print("SAVING TEST BATCH TO: {}".format(image_path))
-    else:
-        print("SAVING TRAIN BATCH TO: {}".format(image_path))
-    cv2.imwrite(image_path, image)
-    """
-    if train:
-        iteration_dir = os.path.join(show_dir, "train", str(iteration))
-    else:
-        iteration_dir = os.path.join(show_dir, "test", str(iteration))
-    if not os.path.exists(iteration_dir):
-        os.mkdir(iteration_dir)
-
-    counter = 0
-    for targets, outputs, paths in zip(all_targets, all_outputs, all_paths):
-        for target, output, path in zip(targets, outputs, paths):
-            target = target.cpu().numpy()
-            output = output.cpu().numpy()
-            t = np.arange(0, 1, 0.001)
-
-            plt.plot(t, target, color='red', label='target')
-            plt.plot(t, output, color='blue', label='output')
-            plt.legend()
-            plt.savefig(os.path.join(iteration_dir, '{}.pdf'.format(path)))
-            plt.cla()
-            if train:
-                counter += 1
-            if counter == 10:
-                return
-
-
-def init_show_dirs(show_dir):
-    trn_dir_path = os.path.join(show_dir, "train")
-    if not os.path.exists(trn_dir_path):
-        os.makedirs(trn_dir_path)
-    tst_dir_path = os.path.join(show_dir, "test")
-    if not os.path.exists(tst_dir_path):
-        os.makedirs(tst_dir_path)
 
 
 def load_weights(training, in_checkpoint=None, start_iteration=0, checkpoint_dir=None):